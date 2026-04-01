533d326db5c493b2c7bc33134df8ad28cd1b1f5c
Renu Negi <renunegi@Renus-MacBook-Air.local>
2026-04-01 11:49:17 +0530

    Clean Terraform project (removed binaries)

---
+++ main.tf
@@ -0,0 +1,64 @@
+# key-pair (login to ec2 instance using ssh)
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "terra-key"
+  public_key = file("terra-key.pub")
+}
+
+# vpc (virtual private cloud) & security group (firewall rules)
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "default-vpc"
+  }
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "automate-sg"
+  description = " this will add a TF generated security group to the default VPC"
+  vpc_id      = aws_default_vpc.default.id # interpolation syntax to get the default vpc id
+
+  # inbound rule to allow ssh access to the ec2 instance from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow ssh access from anywhere"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow http access from anywhere"
+  }
+  # outbound rule to allow all outbound traffic from the ec2 instance to anywhere
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow all outbound traffic to anywhere"
+  }
+}
+
+# ec2 instance
+resource "aws_instance" "my_ec2_instance" {
+  ami             = "ami-0ec10929233384c7f" # ubuntu ami in us-east-1 region
+  instance_type   = "t3.micro"
+  key_name        = aws_key_pair.my_key_pair.key_name
+  security_groups = [aws_security_group.my_security_group.name]
+
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+
+  }
+
+  tags = {
+    Name = "terra-ec2-instance"
+  }
+
+}
